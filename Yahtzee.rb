3a03508285b7ae0b0c4c71c3ea2eaf22b7b247e4
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-16 17:22:40 -0500

    now scores ones

--- Yahtzee.rb
+++ Yahtzee.rb
@@ -1,10 +1,37 @@
 
 class YahtzeeRoll
 
-attr_accessor :roll
+attr_accessor :roll, :score
 
 	def initialize(v, w, x, y, z)
 		@roll = [v, w, x, y, z]
+		@v = v
+		@w = w
+		@x = x
+		@y = y
+		@z = z
+	end
+
+	def score(options = {})
+		if options = [:ones]
+			@roll.select!{ |a| a == 1 }
+			@roll.count
+		else
+			return nil
+		end
+
+
+
+
+		# && (@v+@w+@x+@y+@z) <= 5
+		# 	@roll.delete(1)
+		# 	score = (5 - @roll.count)
+		# 	return score
+		# else
+		# 	puts "no ones"
+
+
+
 	end
 
 end
--- spec/Yahtzee_spec.rb
+++ spec/Yahtzee_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 require_relative '../Yahtzee'
 
-
-
 describe "#YahtzeeRoll" do
+
+	before(:each) do
+		@yahtzee_test1 = YahtzeeRoll.new(1,1,1,1,1)
+		@yahtzee_test2 = YahtzeeRoll.new(2,1,3,3,1)
+	end
+
 	describe "#initialize" do
 
-		yahtzee_test = YahtzeeRoll.new(1,1,1,1,1)
+		it "initializes a roll of 5 dice" do
+			expect(@yahtzee_test1.roll).to eq [1,1,1,1,1]
+		end
+	end
 
-		it "scores rolls" do
-			expect(yahtzee_test.roll).to eq [1,1,1,1,1]
+	describe '#score' do
+		it "scoress ones correctly" do
+			expect(@yahtzee_test1.score(:ones)).to eq 5
+			expect(@yahtzee_test2.score(:ones)).to eq 2
+		end
+
+		it "scoress ones correctly" do
+			expect(@yahtzee_test1.score(:ones)).to eq 5
+			expect(@yahtzee_test2.score(:ones)).to eq 2
 		end
 	end
 end
