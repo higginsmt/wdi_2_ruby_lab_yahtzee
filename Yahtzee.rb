5ce8a73a3de45752dde9aee27a9ed6ba5e3f8ec1
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-16 17:41:50 -0500

    have tests for more funcitons

--- Yahtzee.rb
+++ Yahtzee.rb
@@ -16,26 +16,18 @@ attr_accessor :roll, :score
 		if options = [:ones]
 			@roll.select!{ |a| a == 1 }
 			@roll.count
+		elsif options = [:twos]
+			@roll.select!{ |a| a == 2 }
+			((@roll.count) * 2)
 		else
 			return nil
 		end
-
-
-
-
-		# && (@v+@w+@x+@y+@z) <= 5
-		# 	@roll.delete(1)
-		# 	score = (5 - @roll.count)
-		# 	return score
-		# else
-		# 	puts "no ones"
-
-
-
 	end
-
 end
 
 
 
-
+# elsif options = [:twos]
+# 			@roll.delete(2)
+# 			score = ((5 - @roll.count.length) * 2)
+# 			return score
--- spec/Yahtzee_spec.rb
+++ spec/Yahtzee_spec.rb
@@ -6,6 +6,10 @@ describe "#YahtzeeRoll" do
 	before(:each) do
 		@yahtzee_test1 = YahtzeeRoll.new(1,1,1,1,1)
 		@yahtzee_test2 = YahtzeeRoll.new(2,1,3,3,1)
+		@yahtzee_test3 = YahtzeeRoll.new(2,2,2,3,1)
+		@yahtzee_test4 = YahtzeeRoll.new(2,4,4,3,1)
+		@yahtzee_test5 = YahtzeeRoll.new(2,4,5,5,1)
+		@yahtzee_test6 = YahtzeeRoll.new(6,4,5,5,6)
 	end
 
 	describe "#initialize" do
@@ -21,10 +25,31 @@ describe "#YahtzeeRoll" do
 			expect(@yahtzee_test2.score(:ones)).to eq 2
 		end
 
-		it "scoress ones correctly" do
-			expect(@yahtzee_test1.score(:ones)).to eq 5
-			expect(@yahtzee_test2.score(:ones)).to eq 2
+		it "scoress twos correctly" do
+			expect(@yahtzee_test2.score(:twos)).to eq 2
+			expect(@yahtzee_test3.score(:twos)).to eq 6
 		end
+
+		# it "scoress threes correctly" do
+		# 	expect(@yahtzee_test2.score(:threes)).to eq 6
+		# 	expect(@yahtzee_test3.score(:threes)).to eq 3
+		# end
+
+		# it "scoress fours correctly" do
+		# 	expect(@yahtzee_test2.score(:fours)).to eq 0
+		# 	expect(@yahtzee_test4.score(:fours)).to eq 8
+		# end
+
+		# it "scoress fives correctly" do
+		# 	expect(@yahtzee_test1.score(:fives)).to eq 0
+		# 	expect(@yahtzee_test5.score(:fives)).to eq 10
+		# end
+
+		# it "scoress sixes correctly" do
+		# 	expect(@yahtzee_test1.score(:sixes)).to eq 0
+		# 	expect(@yahtzee_test6.score(:sixes)).to eq 12
+		# end
+
 	end
 end
 
